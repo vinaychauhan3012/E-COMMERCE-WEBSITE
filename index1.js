114d0adbc0de11ee07c05e3eb4290453d5d25f02
vinaychauhan3012 <vinaychauhan3012@gmail.com>
2020-11-08 01:52:30 +0530

    Second Commit

--- index1.js
+++ index1.js
@@ -1,11 +1,11 @@
 // initialization of product img variable
-let productImg=document.getElementById("productImage");
-let smallImg=document.getElementsByClassName("smallImage");
+let productImg = document.getElementById("productImage");
+let smallImg = document.getElementsByClassName("smallImage");
 
 // -----------------product details ------------------------//
 for (let iterator of smallImg) {
-    iterator.addEventListener("click",()=>{
-        productImg.src=iterator.src;
+    iterator.addEventListener("click", () => {
+        productImg.src = iterator.src;
     })
-    
+
 }
